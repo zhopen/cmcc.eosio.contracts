ba7c2b141b59e162bdce15df4e93444eeba7e7a9
vlbos <vlbos2018@gmail.com>
2019-10-18 10:02:33 +0800

    add parameter comment

--- contracts/bos.oracle/include/bos.oracle/tables/provider.hpp
+++ contracts/bos.oracle/include/bos.oracle/tables/provider.hpp
@@ -145,7 +145,7 @@ struct oracle_parameters {
    uint64_t min_reg_arbitrator_stake_limit = default_min_reg_arbitrator_stake_limit;
    uint16_t arbitration_correct_rate = default_arbitration_correct_rate;
    uint8_t round_limit = default_round_limit;
-   uint32_t arbi_timeout_value = default_arbi_timeout_value;                 
+   uint32_t arbi_timeout_value = default_arbi_timeout_value;                  /// 
    uint32_t arbi_freeze_stake_duration = default_arbi_freeze_stake_duration; 
    uint32_t time_deadline = default_time_deadline;                           /// request time deadline or update time deadline
    uint32_t clear_data_time_length = default_clear_data_time_length;         ///   time length  of expiry of data to be cleared
